538d32f938063c9944e145e34c7fa999a1d65f58
unknown <abduldiawade777@gmail.com>
2024-04-30 20:13:16 +0000

    mis à jour du code par diawando

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
 print("Bonjour à Tous")
+
+
+# Code ajouté par Diawando
+# petite calculatrice interactive
+
+valeur1 = int(input("Ajouter une valeur de 0 à 9 : "))
+valeur2 = int(input("Ajouter une valeur de 0 à 9 : "))
+print("la somme des deux valeurs = ", valeur1 + valeur2)
+
