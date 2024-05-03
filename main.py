bab6482015affad9dc446fa5b066f8fb6626db80
Mamadou Dian DIALLO <9006@EDG-SA.LAN>
2024-05-03 16:51:14 +0000

    update

--- main.py
+++ main.py
@@ -1,10 +1,26 @@
-print("Bonjour à Tous")
+print("Bonjour je présente mon Prémier Code en PYTHON")
 
 
 # Code ajouté par Diawando
 # petite calculatrice interactive
 
-valeur1 = int(input("Ajouter une valeur de 0 à 9 : "))
+"""valeur1 = int(input("Ajouter une valeur de 0 à 9 : "))
 valeur2 = int(input("Ajouter une valeur de 0 à 9 : "))
-print("la somme des deux valeurs = ", valeur1 + valeur2)
+print("la somme des deux valeurs = ", valeur1 + valeur2)"""
+
+# Code ajouté par DIAN
+print("Je suis DIALLO MAMADOU DIAN")
+agePersonne = 31    
+print("Mon âge est :", agePersonne ,"ANS" )
+# Un code intereactif 
+
+filiation =input("Quel est votre nom ?: ")
+print("Mon nom est:", filiation)
+age1 = input("Quel est votre Âge ?: ")
+print("J'ai ",age1,"ANS Commencer à faire votre addition")
+25
+nombre1 = int(input("entrez un nombre de 0 à 9 :" ))
+nombre2 = int(input("entrez un nombre de 0 à 9 :" ))
+print("la somme est:", nombre1+nombre2, "Merci", filiation)
+
 
